cf03206b6b9ea1281558bf5da8f2582802914e34
naveenout <na78019@gmail.com>
2021-11-22 22:20:00 +0400

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,3 +1,6 @@
+#Supervised Learning - Linear Regression
+#Simple Example to understand Linear Regression
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
